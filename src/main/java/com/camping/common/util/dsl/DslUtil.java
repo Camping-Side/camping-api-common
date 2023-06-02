b4e81153a9a95cf27614d980d7c134e138083a68
dkask <dkaskgkdua@gmail.com>
2023-06-02 12:35:36 +0900

    feat: DslUtil 개선
    - Dsl 비교 유틸 오버로딩 적용

--- src/main/java/com/camping/common/util/dsl/DslUtil.java
+++ src/main/java/com/camping/common/util/dsl/DslUtil.java
@@ -1,21 +1,40 @@
 package com.camping.common.util.dsl;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.EnumPath;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.util.StringUtils;
 
 public class DslUtil {
-    public static BooleanExpression cprNumEq(NumberPath<Integer> target, Integer number) {
+    /**
+     * Integer 타입 값 = 비교
+     */
+    public static BooleanExpression cprEq(NumberPath<Integer> target, Integer number) {
         return number == null ? null : target.eq(number);
     }
-    public static BooleanExpression cprNumEq(NumberPath<Long> target, Long number) {
+    /**
+     * Long 타입 값 = 비교
+     */
+    public static BooleanExpression cprEq(NumberPath<Long> target, Long number) {
         return number == null ? null : target.eq(number);
     }
-    public static BooleanExpression cprStrEq(StringPath target, String str) {
+    /**
+     * String 타입 값  = 비교
+     */
+    public static BooleanExpression cprEq(StringPath target, String str) {
         return StringUtils.hasText(str) ? target.eq(str) : null;
     }
-    public static BooleanExpression cprStrLike(StringPath target, String str) {
+    /**
+     * String 타입 값  like 비교
+     */
+    public static BooleanExpression cprLike(StringPath target, String str) {
         return StringUtils.hasText(str) ? target.like("%" + str + "%") : null;
+    }
+    /**
+     * Enum 타입 값  = 비교
+     */
+    public static <T extends Enum<T>> BooleanExpression cprEq(EnumPath<T> target, T enumObject) {
+        return enumObject == null ? null : target.eq(enumObject);
     }
 }
